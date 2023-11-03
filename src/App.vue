9328033be7327288140e0d1875e9246773921fae
manish.devstree <manish.devstree@gmail.com>
2023-11-03 18:40:17 +0530

    introduction

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <About/>
 </template>
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import About from './components/About.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    About,
+  } 
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  *{
+    text-align: center  ;
+  }
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,89 @@
+<template>
+ <h1>Hello manish About that you</h1>
+ <h2>Email {{ email  }}</h2>
+ <h2>Name {{ Name }}</h2>
+ <h2>Mo.No {{ Mobile  }}</h2>
+ <h2>Role {{ Role}}</h2>
+ <h5>My name {{ myName("Manish bhai Rathva") }}</h5>
+ <h4>full name {{ getName("Manu")  }}</h4>
+ <h4>first Name {{ getData().name}}</h4>
+ <div class="btn">
+    <button class="btn1" v-on:click="tested()">Submit</button>
+ </div>
+ <p>{{ count }}</p>
+
+ <h1 v-bind:class="vueClass">Patil</h1>
+<form>
+  <label>email:</label>
+  <input v-model="email" type="text"><br>
+  <br>
+  <label>lname:</label>
+  <input v-model="Name" type="text"><br>
+  <br>
+  <button v-on:click="clickMe()" type="button">Get Data</button>
+</form>
+
+</template>
+
+<script>
+export default {
+  name: 'AboutData',
+  data(){
+    return{
+        email:null,
+        Name:null,
+        Mobile:123456789,
+        Role:'Admin',
+        count:0,
+        vueClass:'vueClass',
+        myName(name){
+        return name;
+       }
+    }
+  },
+  methods:{
+    getName(fullname){
+        return fullname;
+    },
+    getData(){
+        return{
+            name:'Manish',
+            lastname:'Rathva'
+        }
+    },
+    tested(){
+     this.count = this.count+1;  
+    },
+    clickMe(){
+      console.warn("get data",this.email,this.Name)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+*{
+    text-align: center;
+}
+.btn{
+    align-items: center;
+    justify-content: center;
+}
+.btn1{
+    padding:0.5rem;
+    background-color: green;
+    color: white;   
+    font-size:17px;
+    border:1px solid black;
+    border-radius: 19px;
+}
+h1{
+    color: red;
+}
+
+.vueClass{
+    background-color: yellow;
+}
+
+</style>
